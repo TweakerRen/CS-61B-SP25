1da56381100876aae6bfd9e6b5c99322e59306a9
Yangjie Ren <cyigy3@163.com>
2025-08-23 18:46:01 +0800

    Aug 23 2025

--- lab03/src/adventure/SpeciesListStage.java
+++ lab03/src/adventure/SpeciesListStage.java
@@ -101,6 +101,9 @@ public class SpeciesListStage implements AdventureStage {
                 copy.remove(o);
             }
         }
-        return similarObjects / listOne.size();
+        if (listOne.size() != 0) {
+            return similarObjects / listOne.size();
+        }
+        return 0;
     }
 }
